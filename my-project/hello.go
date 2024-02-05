8eee098a21bfbd4f67edb0cb45546e4f1e3fee54
Arman Sydikov <sydikov.arman@gmail.com>
2024-02-05 20:09:27 +0600

    Hello, world... again

--- my-project/hello.go
+++ my-project/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello returns a greeting.
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
