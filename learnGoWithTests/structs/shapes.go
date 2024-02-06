02a939078c9942c890aab61279abf84412b9577a
Arman Sydikov <sydikov.arman@gmail.com>
2024-02-06 18:22:51 +0600

    Structs/Refactor

--- learnGoWithTests/structs/shapes.go
+++ learnGoWithTests/structs/shapes.go
@@ -4,6 +4,9 @@ type Rectangle struct {
 	Width  float64
 	Height float64
 }
+type Circle struct {
+	Radius float64
+}
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
